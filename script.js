70d47c5c832b3d6e1b891d956cd43f20dfd80a83
ammartalpur <ammartalpur69@gmail.com>
2025-06-13 20:14:50 +0500

    learned about array , object and some its functions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+// const person = {
+//   name: "Ammar Talpur",
+//   age: 22,
+//   greet: function () {
+//     console.log(`Hello ${this.name}`);
+//   }
+// }
+
+
+// console.log(person.greet())
+
+// let colors = ["red", "green", "blue"]
+
+// for (let color of colors) {
+//   console.log(color)
+// }
+
+
+let student = {
+  name: "ammar",
+  age: 20
+}
+
+for (let key in student) {
+  console.log("Key: " + key);
+  console.log(student[key])
+  console.log("--------------");
+}
